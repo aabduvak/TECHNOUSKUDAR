d8f157f672614ca355c7692a73c7f44e23a323e4
abdulaziz <abdulaziz.yosk@gmail.com>
2021-03-10 08:53:33 +0300

    added techpost.scss and slider.js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,7 @@
 import VideoPlayer from './modules/videoPlayer';
 import Menu from './modules/menu';
 import Forms from './modules/forms';
-// import Sliders from './modules/slider';
+import Sliders from './modules/slider';
 import Splide from '@splidejs/splide';
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -15,6 +15,7 @@ window.addEventListener("DOMContentLoaded", () => {
     } else {
         new Menu('.other-header .hamburger', '.other-header .menu', '.other-header .menu__close', '.other-header .menu__overlay').init();
         new Forms().init();
+        new Sliders('.techpost__item', 2000).init();
         new Splide( '.splide', {
             type   : 'loop',
             perPage: 2,
