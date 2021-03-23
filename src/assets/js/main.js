24d813464fe4307693f8f29b4ef8469cb3b186d3
abdulaziz <abdulaziz.yosk@gmail.com>
2021-03-23 21:45:44 +0300

    added extra.scss, changed img.height in posts

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,28 +2,17 @@ import VideoPlayer from './modules/videoPlayer';
 import Menu from './modules/menu';
 import Forms from './modules/forms';
 import Sliders from './modules/slider';
-import Splide from '@splidejs/splide';
 
 window.addEventListener("DOMContentLoaded", () => {
     try {
         new VideoPlayer('.play', '.overlay').init();
-    }catch(e){}    
-
-    if (document.querySelector('.promo')) {
-        new Menu('.promo .hamburger', '.promo .menu', '.promo .menu__close', '.promo .menu__overlay').init();
-        
-    } else {
-        new Menu('.other-header .hamburger', '.other-header .menu', '.other-header .menu__close', '.other-header .menu__overlay').init();
         new Forms().init();
-        new Sliders('.techpost__item', 2000).init();
-        new Splide( '.splide', {
-            type   : 'loop',
-            perPage: 2,
-            perMove: 1,
-            focus  : 'center',
-            autoplay: true,
-        } ).mount();
-    }
-
-
+            new Sliders('.blocks-gallery-item', 4000).init();
+        if (document.querySelector('.promo')) {
+            new Menu('.promo .hamburger', '.promo .menu', '.promo .menu__close', '.promo .menu__overlay').init();
+            
+        } else {
+            new Menu('.other-header .hamburger', '.other-header .menu', '.other-header .menu__close', '.other-header .menu__overlay').init();
+        }
+    }catch(e){}    
 });
