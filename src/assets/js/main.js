ab783d244de10519b6c2c393ecd1a262f8474b8c
abdulaziz <abdulaziz.yosk@gmail.com>
2021-04-10 14:57:00 +0300

    created sponsors.scss

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,13 +1,11 @@
 import VideoPlayer from './modules/videoPlayer';
 import Menu from './modules/menu';
-import Forms from './modules/forms';
 import Sliders from './modules/slider';
 
 window.addEventListener("DOMContentLoaded", () => {
     try {
         new VideoPlayer('.play', '.overlay').init();
-        new Forms().init();
-            new Sliders('.blocks-gallery-item', 4000).init();
+        new Sliders('.blocks-gallery-item', 4000).init();
         if (document.querySelector('.promo')) {
             new Menu('.promo .hamburger', '.promo .menu', '.promo .menu__close', '.promo .menu__overlay').init();
             
