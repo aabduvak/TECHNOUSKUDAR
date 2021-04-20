d9c82e4f296adaa579d237ce317c2f388ccf8266
abdulaziz <abdulaziz.yosk@gmail.com>
2021-04-20 20:05:13 +0300

    media.scss is modified

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,14 +3,14 @@ import Menu from './modules/menu';
 import Sliders from './modules/slider';
 
 window.addEventListener("DOMContentLoaded", () => {
+    if (document.querySelector('.promo')) {
+        new Menu('.promo .hamburger', '.promo .menu', '.promo .menu__close', '.promo .menu__overlay').init();
+        
+    } else {
+        new Menu('.other-header .hamburger', '.other-header .menu', '.other-header .menu__close', '.other-header .menu__overlay').init();
+    }
     try {
         new VideoPlayer('.play', '.overlay').init();
-        new Sliders('.blocks-gallery-item', 4000).init();
-        if (document.querySelector('.promo')) {
-            new Menu('.promo .hamburger', '.promo .menu', '.promo .menu__close', '.promo .menu__overlay').init();
-            
-        } else {
-            new Menu('.other-header .hamburger', '.other-header .menu', '.other-header .menu__close', '.other-header .menu__overlay').init();
-        }
+        new Sliders('.blocks-gallery-item', 3000).init();  
     }catch(e){}    
 });
